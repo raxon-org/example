134da010225f751ce50bfb0fcd64a95a845ab416
Remco van der Velde <remco@universeorange.com>
2026-06-06 14:36:45 +0200

    init

--- src/Domain/Example.Com/Public/Js/Priya/0.3.18/Bin/Priya.prototype.js
+++ src/Domain/Example.Com/Public/Js/Priya/0.3.18/Bin/Priya.prototype.js
@@ -31,6 +31,7 @@ var priya = function (collection){
     this.collect.parameters = collection.parameters;
     this.collect.require = {};
     this.collect.require.jid = 1;
+    console.log(this.collect);
     var jid = this.collect.require.jid;
     let agent = navigator.userAgent;
     let needle = 'SamsungBrowser/';
