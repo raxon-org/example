247cd8de58ba2c87f47717a36c3ecb5259834deb
Remco van der Velde <remco@universeorange.com>
2026-06-06 14:44:49 +0200

    init

--- src/Domain/Example.Com/Public/Js/Priya/0.3.18/Priya.js
+++ src/Domain/Example.Com/Public/Js/Priya/0.3.18/Priya.js
@@ -122,16 +122,17 @@ var priya;
     src = src[0].split('/');
     src.pop();
     src = src.join('/') + '/';
-    console.log(src);   
-    var bin = src + 'Bin/';
-    var node = include(bin + 'Priya.prototype.js');
-    console.log(bin);
-    node.addEventListener('load', function(event){
-        priya = new priya({
-            "url" : url,
-            "bin" : bin,
-            "priya" : src,
-            "parameters" : parameters
-        });
-    }, false);
+    if(!priya){
+        var bin = src + 'Bin/';
+        var node = include(bin + 'Priya.prototype.js');
+        console.log(bin);
+        node.addEventListener('load', function(event){
+            priya = new priya({
+                "url" : url,
+                "bin" : bin,
+                "priya" : src,
+                "parameters" : parameters
+            });
+        }, false);
+    }
 })();
